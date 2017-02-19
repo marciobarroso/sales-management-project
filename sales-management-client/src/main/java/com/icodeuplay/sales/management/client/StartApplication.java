765ac949736f68bfca42ef8647eec1bd45aadde9
Marcio Barroso <mbarroso@inmetrics.com.br>
2017-02-19 00:11:21 -0300

    create login interface

--- sales-management-client/src/main/java/com/icodeuplay/sales/management/client/StartApplication.java
+++ sales-management-client/src/main/java/com/icodeuplay/sales/management/client/StartApplication.java
@@ -1,5 +1,36 @@
 package com.icodeuplay.sales.management.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.icodeuplay.base.view.swing.containers.ApplicationContainer;
+import com.icodeuplay.base.view.swing.util.LookAndFeelSelector;
+import com.icodeuplay.sales.management.client.screen.LoginScreen;
+
 public class StartApplication {
+
+	private ApplicationContainer container;
+
+	private static Logger LOGGER = LoggerFactory.getLogger(StartApplication.class);
+
+	private StartApplication() {
+		try {
+
+			this.container = ApplicationContainer.getInstance();
+			this.container.addPanel(new LoginScreen());
+
+		} catch (Exception e) {
+			LOGGER.error("Error initialize StartApplication", e);
+		}
+	}
+
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				LookAndFeelSelector.setBusinessBluesSteel();
+				new StartApplication();
+			}
+		});
+	}
 
 }
